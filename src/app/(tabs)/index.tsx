ccf820d8af5f5ee34a7d4d7c80edbbcefdb9ce01
haikaljs <haikaljavascript@gmail.com>
2026-05-17 00:29:25 +0800

    Header component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import Header from "@/components/Header";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Home() {
   return (
-    <SafeAreaView className='flex-1' edges={['top']}>
-      <Text>Home</Text>
+    <SafeAreaView className="flex-1" edges={["top"]}>
+      <Header title='forever' showMenu showCart showLogo/>
     </SafeAreaView>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
   content: ["./src/app/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#111111",
+        secondary: "#666666",
+        background: "#FFFFFF",
+        surface: "#F7F7F7",
+        accent: "#FF4C3B",
+        border: "#EEEEEE"
+      }
+    },
   },
   plugins: [],
 }
